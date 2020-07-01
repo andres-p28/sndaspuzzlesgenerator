21370b1369bd3e27de2cab6059464c9232f6f7b9
Andres Palumbo <andrespalumbo@gmail.com>
2020-07-01 03:20:06 -0300

    Updates

---
+++ precache-manifest.50e82f77761e79faa4f20a7deb72cd05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdab2c00ff7103779e2664089a3f5bf1",
+    "url": "/sndaspuzzlesgenerator/index.html"
+  },
+  {
+    "revision": "d355c45d09dd125bb0f0",
+    "url": "/sndaspuzzlesgenerator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d3420b7d24452653d064",
+    "url": "/sndaspuzzlesgenerator/static/js/2.d608b861.chunk.js"
+  },
+  {
+    "revision": "07a9a189a721a74922a426fba0803a30",
+    "url": "/sndaspuzzlesgenerator/static/js/2.d608b861.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d355c45d09dd125bb0f0",
+    "url": "/sndaspuzzlesgenerator/static/js/main.34cfdfcc.chunk.js"
+  },
+  {
+    "revision": "c2153840ededa3f7ab31",
+    "url": "/sndaspuzzlesgenerator/static/js/runtime-main.13a90a41.js"
+  }
+]);
